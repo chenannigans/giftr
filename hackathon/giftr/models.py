e58d189918c6a64cfcc5f854d8f9214b37123f7b
Ann Chen <ann.chen@capitalone.com>
2016-07-06 16:08:33 -0400

    models

--- hackathon/giftr/models.py
+++ hackathon/giftr/models.py
@@ -3,3 +3,13 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Gift(models.Model):
+	user = models.ForeignKey(User)
+	picture = models.FileField(upload_to=user_directory_path)
+	description = models.CharField(max_length=100)
+	price = models.DateTimeField(max_digits=None, decimal_places = 2)
+	url = models.CharField(max_length=None)
+	category = models.CharField(max_length=None)
+	recipient_category = models.CharField(max_length=None)
+
