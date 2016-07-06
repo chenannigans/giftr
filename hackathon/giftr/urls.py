0b63f5d480c8300e533d3e19e7c1eeaa1e568d00
Ann Chen <ann.chen@capitalone.com>
2016-07-06 17:20:28 -0400

    merge conflict changes

--- hackathon/giftr/urls.py
+++ hackathon/giftr/urls.py
@@ -3,10 +3,7 @@ from django.conf.urls import include, url
 urlpatterns = [
 	url(r'^$', 'giftr.views.hello_world'),
     url(r'^hello-world$', 'giftr.views.hello_world'),
-<<<<<<< HEAD
     url(r'^make$', 'giftr.views.make'),
     # url(r'^hello.html$', 'intro.views.hello'),
-=======
     url(r'^populate$', 'giftr.views.populate'),
->>>>>>> ea5d1b1d4f0ae2fb5b24e62a517e34f33ac4ccff
 ]
