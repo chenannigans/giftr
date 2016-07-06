4b588a1bb01cde124a73027a366ccd76b3b92448
Park <seongyeo@andrew.cmu.edu>
2016-07-06 17:11:51 -0400

    changes

--- hackathon/giftr/urls.py
+++ hackathon/giftr/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import include, url
 urlpatterns = [
 	url(r'^$', 'giftr.views.hello_world'),
     url(r'^hello-world$', 'giftr.views.hello_world'),
+    url(r'^make$', 'giftr.views.make'),
     # url(r'^hello.html$', 'intro.views.hello'),
 ]
