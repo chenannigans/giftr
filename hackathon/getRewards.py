403bebab1344996e32359a744b6803677aa34280
elee020307 <el532@cornell.edu>
2016-07-14 22:47:04 -0400

    Changed imports in getRewards.py

--- hackathon/getRewards.py
+++ hackathon/getRewards.py
@@ -1,12 +1,8 @@
 import time
 import requests
 import sys
-try:
-    # For Python 3.0 and later
-    import urllib.parse as urlparse
-except ImportError:
-    # Fall back to Python 2's urlparse
-    import urlparse
+import urlparse
+import urllib
 
 CLIENT_ID = "enterpriseapi-sb-0iSeXHHzheNu1AzI7DJbzea7"
 CLIENT_SECRET = "0be2610f200b69fdd1699084822cae2a96183d45"
@@ -14,118 +10,119 @@ REDIRECT_URL = "http://localhost:8000/giftr/login_only"
 cardNumber = 8729
 rewardsAccountReferenceId = None
 
-
 global auth_token
 global prev_time
 global rewards
 
 
 def getCodeFromUrl(url):
-	parsed = urlparse.parse_qs(urlparse.urlparse(url).query)
-	print(parsed)
-	if 'code' in parsed:
-		return parsed['code'][0]
-	return None
+    parsed = urlparse.parse_qs(urlparse.urlparse(url).query)
+    print(parsed)
+    if 'code' in parsed:
+        return parsed['code'][0]
+    return None
+
 
 def getAccessToken(code):
-	global auth_token
-	global prev_time
+    global auth_token
+    global prev_time
 
-#response = requests.get('https://api.spotify.com/v1/albums/0sNOF9WDwhWunNAHPD3Baj')
-#	print(response.json())
+    # response = requests.get('https://api.spotify.com/v1/albums/0sNOF9WDwhWunNAHPD3Baj')
+    #	print(response.json())
 
-	headers = {
-    'Content-Type': 'application/x-www-form-urlencoded'
-	}
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
 
-	data = 'code='+code+'&client_id='+CLIENT_ID+'&client_secret='+CLIENT_SECRET+'&grant_type=authorization_code&redirect_uri='+REDIRECT_URL
+    data = 'code=' + code + '&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET + \
+           '&grant_type=authorization_code&redirect_uri=' + REDIRECT_URL
 
-	response = requests.post('https://api-sandbox.capitalone.com/oauth/oauth20/token', headers=headers, data=data)
-	print(response.ok)
-	print(response.text)
-	auth_token = response.json()
-	prev_time = time.time()			
-	return response.json()
+    response = requests.post('https://api-sandbox.capitalone.com/oauth/oauth20/token', headers=headers, data=data)
+    print(response.ok)
+    print(response.text)
+    auth_token = response.json()
+    prev_time = time.time()
+    return response.json()
 
 
 def refreshAccessToken():
-	global auth_token
-	global prev_time
-	refresh_token = auth_token['refresh_token']
+    global auth_token
+    global prev_time
+    refresh_token = auth_token['refresh_token']
 
-	headers = {
-    'Content-Type': 'application/x-www-form-urlencoded'
-	}
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
 
-	data = 'client_id='+CLIENT_ID+'&client_secret='+CLIENT_SECRET+'&grant_type=refresh_token&refresh_token='+refresh_token
+    data = 'client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET + \
+           '&grant_type=refresh_token&refresh_token=' + refresh_token
 
-	response = requests.post('https://api-sandbox.capitalone.com/oauth/oauth20/token', headers=headers, data=data)
-	auth_token = response.json()	
-	prev_time = time.time()
+    response = requests.post('https://api-sandbox.capitalone.com/oauth/oauth20/token', headers=headers, data=data)
+    auth_token = response.json()
+    prev_time = time.time()
 
-
-	return response.json()
+    return response.json()
 
 
 def refresh():
-	if time.time() + 10 > prev_time + auth_token['expires_in']:
-		refreshAccessToken()
-
+    if time.time() + 10 > prev_time + auth_token['expires_in']:
+        refreshAccessToken()
 
 
 def getRewardsAccounts():
-	global rewardsAccountReferenceId
+    global rewardsAccountReferenceId
 
-	refresh()
-	headers = {
-    'Accept': 'application/json;v=1',
-    'Authorization': 'Bearer '+ auth_token['access_token']
-	}
+    refresh()
+    headers = {
+        'Accept': 'application/json;v=1',
+        'Authorization': 'Bearer ' + auth_token['access_token']
+    }
 
-	url = 'https://api-sandbox.capitalone.com/rewards/accounts?creditCardLastFour='+str(cardNumber)
+    url = 'https://api-sandbox.capitalone.com/rewards/accounts?creditCardLastFour=' + str(cardNumber)
 
-	response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers)
 
-	if response.ok and "rewardsAccounts" in response.json():
-		rewardsAccountReferenceId = urlparse.quote_plus(response.json()['rewardsAccounts'][0]['rewardsAccountReferenceId'])
-
+    if response.ok and "rewardsAccounts" in response.json():
+        rewardsAccountReferenceId = urllib.quote_plus((
+            response.json()['rewardsAccounts'][0]['rewardsAccountReferenceId']))
 
 
 def getRewardsAccountsDetails():
-	getRewardsAccounts()
-	print(rewardsAccountReferenceId)
+    getRewardsAccounts()
+    print(rewardsAccountReferenceId)
 
-	headers = {
-    'Accept': 'application/json;v=1',
-    'Authorization': 'Bearer '+ auth_token['access_token']
-	}
+    headers = {
+        'Accept': 'application/json;v=1',
+        'Authorization': 'Bearer ' + auth_token['access_token']
+    }
 
-	response = requests.get('https://api-sandbox.capitalone.com/rewards/accounts/'+rewardsAccountReferenceId, headers=headers)
-	print(response.ok)
-	print(response.json())
-	return response.json()
+    response = requests.get('https://api-sandbox.capitalone.com/rewards/accounts/' + rewardsAccountReferenceId,
+                            headers=headers)
+    print(response.ok)
+    print(response.json())
+    return response.json()
+
 
 def authenticate(url):
-# # 	#go to authenticate website
-	# grab url
-	#url = ""
-	getAccessToken(url)
+    # # 	#go to authenticate website
+    # grab url
+    # url = ""
+    getAccessToken(url)
 
-	while True:
-		#check if token needs to be refreshed 
-		
-		if (time.time() + 10) > (prev_time + auth_token['expires_in']):
-			print ("refreshing token")
-			refreshAccessToken()
+    while True:
+        # check if token needs to be refreshed
+
+        if (time.time() + 10) > (prev_time + auth_token['expires_in']):
+            print ("refreshing token")
+            refreshAccessToken()
 
 
 def redeemMoney():
-	rewardsDetails = getRewardsAccountsDetails()
-	if rewardsDetails['canRedeem'] and rewardsDetails['rewardsCurrency'] == "Cash":
-		#super lazy for now
-		print("You can save up to " + str(rewardsDetails['rewardsBalance']))
-		return rewardsDetails['rewardsBalance']
-
+    rewardsDetails = getRewardsAccountsDetails()
+    if rewardsDetails['canRedeem'] and rewardsDetails['rewardsCurrency'] == "Cash":
+        # super lazy for now
+        print("You can save up to " + str(rewardsDetails['rewardsBalance']))
+        return rewardsDetails['rewardsBalance']
 
 
 # # paste your url here: e.g. https://www.google.com/?code=idFKpQH1T8RcHVOzFbo-qOlVaDi67_IWd0FpGg
@@ -144,26 +141,17 @@ def redeemMoney():
 
 
 def main():
-	url = sys.argv[1]
-	code = getCodeFromUrl(url)
-	if code is not None:
-		getAccessToken(code)
-		return redeemMoney()
-	return 0
+    url = sys.argv[1]
+    code = getCodeFromUrl(url)
+    if code is not None:
+        getAccessToken(code)
+        return redeemMoney()
+    return 0
 
 
 url = sys.argv[1]
 code = getCodeFromUrl(url)
 if code is not None:
-	getAccessToken(code)
-	rewards = redeemMoney()
+    getAccessToken(code)
+    rewards = redeemMoney()
 rewards = 0
-
-
-
-
-
-
-
-
-
