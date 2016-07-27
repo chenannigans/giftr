1077592fb41334cfa7f310d1bff00a7169a9c499
Alice Park <seongyeo@andrew.cmu.edu>
2016-07-27 10:45:08 -0400

    typo fixed

--- hackathon/giftr/static/giftr.js
+++ hackathon/giftr/static/giftr.js
@@ -1,4 +1,4 @@
 $( document ).ready(function() {
   // Handler for .ready() called.
-  Materialize.toast('Unable to connet to Capital One account :(', 10000)
+  Materialize.toast('Unable to connect to Capital One account :(', 10000)
 });
